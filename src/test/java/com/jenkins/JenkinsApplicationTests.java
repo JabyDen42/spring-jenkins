3a0c874af11c3f01c89810a3180c2706d06003ab
JabyDen42 <dembodiaby92@gmail.com>
2022-12-09 01:42:13 -0600

    first commit

---
+++ src/test/java/com/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	public  static Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
+
+	@Test
+	public void contextLoads() { 
+		logger.info("Test case executing ... ");
+	}
+
+}
